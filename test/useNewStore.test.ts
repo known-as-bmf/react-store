3cfd2ea49b0a594d6b61c52631f08b1836d1d1f2
Julien Avert <avert.julien@gmail.com>
2020-07-08 23:04:55 +0200

    bump deps && add store as thirs item in useNewStore

--- test/useNewStore.test.ts
+++ test/useNewStore.test.ts
@@ -1,11 +1,22 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 
 import { useNewStore } from '../src';
+import { isStore, subscribe } from '@known-as-bmf/store';
 
 describe('useNewStore', () => {
   it('should be a function', () => {
     expect(useNewStore).toBeDefined();
     expect(useNewStore).toBeInstanceOf(Function);
+  });
+
+  it('should return the state as first item', () => {
+    const initialState = 1;
+    const { result } = renderHook(() => useNewStore(initialState));
+
+    expect(result.current).toBeDefined();
+    expect(result.current).toBeInstanceOf(Array);
+    expect(result.current).toHaveLength(3);
+    expect(result.current[0]).toBe(initialState);
   });
 
   it.each([undefined, null, false, true, 0, '', [], {}, () => 'test'])(
@@ -15,18 +26,18 @@ describe('useNewStore', () => {
 
       expect(result.current).toBeDefined();
       expect(result.current).toBeInstanceOf(Array);
-      expect(result.current).toHaveLength(2);
+      expect(result.current).toHaveLength(3);
       expect(result.current[0]).toBe(initialState);
     }
   );
 
-  it('should return a swap function', () => {
+  it('should return a swap function as second item', () => {
     const initialState = 1;
     const { result } = renderHook(() => useNewStore(initialState));
 
     expect(result.current).toBeDefined();
     expect(result.current).toBeInstanceOf(Array);
-    expect(result.current).toHaveLength(2);
+    expect(result.current).toHaveLength(3);
     expect(result.current[0]).toBe(initialState);
     expect(result.current[1]).toBeInstanceOf(Function);
 
@@ -36,8 +47,36 @@ describe('useNewStore', () => {
 
     expect(result.current).toBeDefined();
     expect(result.current).toBeInstanceOf(Array);
-    expect(result.current).toHaveLength(2);
+    expect(result.current).toHaveLength(3);
     expect(result.current[0]).toBe(initialState + 1);
     expect(result.current[1]).toBe(swap);
+  });
+
+  it('should return the store as third item', () => {
+    const initialState = 1;
+    const { result } = renderHook(() => useNewStore(initialState));
+    const callback = jest.fn();
+
+    expect(result.current).toBeDefined();
+    expect(result.current).toBeInstanceOf(Array);
+    expect(result.current).toHaveLength(3);
+    expect(result.current[0]).toBe(initialState);
+    expect(isStore(result.current[2])).toBe(true);
+
+    const store = result.current[2];
+    const swap = result.current[1];
+
+    // subscribe to store to verify that it is the samme as the one modified by swap
+    subscribe(store, callback);
+
+    void act(() => swap((s) => s + 1));
+
+    expect(result.current).toBeDefined();
+    expect(result.current).toBeInstanceOf(Array);
+    expect(result.current).toHaveLength(3);
+    expect(result.current[0]).toBe(initialState + 1);
+    expect(result.current[1]).toBe(swap);
+    expect(result.current[2]).toBe(store);
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 });
